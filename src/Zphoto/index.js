7d5e21633f97a4903267cd35674e921682637b92
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2021-07-20 22:17:26 -0700

    fixed folder structure by removing folder

--- src/Zphoto/index.js
+++
@@ -1,7 +0,0 @@
-const IMAGES = {
-  citySearch: require("../Zphoto/citySearch.png").default,
-  passwordGen: require("../Zphoto/password.jpg").default,
-  spotifyApi: require("../Zphoto/spotify.jpg").default,
-  weatherDash: require("../Zphoto/weatherDash.jpg").default,
-};
-export default IMAGES;
