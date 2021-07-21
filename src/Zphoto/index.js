b8a76a396a65017e5a4aef4e0e2a8736b2c6e130
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2021-07-20 21:57:33 -0700

    fixing files

---
+++ src/Zphoto/index.js
@@ -0,0 +1,7 @@
+const IMAGES = {
+  citySearch: require("../Zphoto/citySearch.png").default,
+  passwordGen: require("../Zphoto/password.jpg").default,
+  spotifyApi: require("../Zphoto/spotify.jpg").default,
+  weatherDash: require("../Zphoto/weatherDash.jpg").default,
+};
+export default IMAGES;
